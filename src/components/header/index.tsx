113e3dd5f19e8f511d720116e8188e493f7ed887
José Caldas <jrcaldas@yahoo.com.br>
2023-12-27 22:50:55 -0300

    add styled-components

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import styles from './Header.module.css'
 import { loginUser, logoutUser } from '../../features/user/userSlice'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import * as S from './styles'
 
 const Header = () => {
   const { currentUser } = useAppSelector(
     (rootReducer) => rootReducer.userReducer
   )
 
-  console.log()
   const dispatch = useAppDispatch()
 
   const handleLoginClick = () => {
@@ -18,17 +16,17 @@ const Header = () => {
     dispatch(logoutUser())
   }
   return (
-    <div className={styles.header_container}>
+    <S.HeaderContainer>
       <p>redux-typescript</p>
       {currentUser && <p>Bem-vindo, José Caldas</p>}
-      <button className={styles.header_btn} type="button">
+      <S.HeaderButton type="button">
         {currentUser ? (
           <div onClick={handleLogoutClick}>Sair</div>
         ) : (
           <div onClick={handleLoginClick}>Login</div>
         )}
-      </button>
-    </div>
+      </S.HeaderButton>
+    </S.HeaderContainer>
   )
 }
 
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,22 +1,23 @@
-.header_container {
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
   background-color: #222222;
   color: #ffffff;
   width: 100%;
-
   display: flex;
   align-items: center;
   justify-content: space-around;
   padding: 1rem;
-}
 
-.header_btn {
+  p {
+    font-size: 1.5rem;
+  }
+`
+
+export const HeaderButton = styled.button`
   background: transparent;
   color: #ffffff;
   border: none;
   font-size: 1.5rem;
   cursor: pointer;
-}
-
-p {
-  font-size: 1.5rem;
-}
+`
